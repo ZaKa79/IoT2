15ca4cf9ac2065d350c932d91f83d36142888555
ZaKa79 <115881704+ZaKa79@users.noreply.github.com>
2023-04-04 10:12:44 +0200

    Removed lines not in use

--- Raspberry Pi/gps2.py
+++ Raspberry Pi/gps2.py
@@ -1,13 +1,11 @@
 import serial
 import time
-import string
 import pynmea2
 
 
 def gps_read():
     port="/dev/ttyAMA0"
     ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
     newdata=ser.readline().decode("unicode_escape")
 
     if newdata[0:6] == "$GPRMC":
